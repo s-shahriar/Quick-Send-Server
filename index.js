e482485e74858a0b697859225e1ee1f70ca0cbee
s-shahriar <ksnkkc@gmail.com>
2024-06-03 16:43:30 +0600

    pagination added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 const app = express();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const corsOptions = {
   origin: [
@@ -144,13 +144,13 @@ async function run() {
           dateOfBirth,
           selectedPackage,
         } = req.body;
-    
+
         // Check if email is already registered
         const existingHRManager = await userCollection.findOne({ email });
         if (existingHRManager) {
           return res.status(400).json({ error: "Email already exists" });
         }
-    
+
         // Insert new HR manager into the database
         const result = await userCollection.insertOne({
           fullName,
@@ -172,8 +172,8 @@ async function run() {
     });
 
     // check user role
-    app.get('/users/:email', async (req, res) => {
-      const email = req.params.email;  // Extract email from request parameters
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email; // Extract email from request parameters
       try {
         const query = { email: email };
         const user = await userCollection.findOne(query);
@@ -189,46 +189,47 @@ async function run() {
     });
 
     // payment intent
-    app.post('/create-payment-intent', async (req, res) => {
+    app.post("/create-payment-intent", async (req, res) => {
       const { price } = req.body;
       const amount = parseInt(price * 100);
 
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
-        currency: 'usd',
-        payment_method_types: ['card']
+        currency: "usd",
+        payment_method_types: ["card"],
       });
 
       res.send({
-        clientSecret: paymentIntent.client_secret
-      })
+        clientSecret: paymentIntent.client_secret,
+      });
     });
 
     // update hr's limit
-    app.post('/update-payment', async (req, res) => {
+    app.post("/update-payment", async (req, res) => {
       try {
         const { email, package, limit } = req.body;
-    
+
         // Validate the input data
         if (!email || !package || !limit) {
           return res.status(400).json({ error: "Invalid input data" });
         }
-    
+
         // Find the existing user by email
         const existingUser = await userCollection.findOne({ email });
-    
+
         // Determine the new limit value
-        const currentLimit = existingUser && existingUser.limit ? existingUser.limit : 0;
+        const currentLimit =
+          existingUser && existingUser.limit ? existingUser.limit : 0;
         const newLimit = currentLimit + parseInt(limit);
-    
+
         // Update the user's payment and limit fields in the database
         const result = await userCollection.updateOne(
           { email },
           { $set: { package, limit: newLimit } }
         );
-    
+
         if (result.modifiedCount > 0) {
-          res.status(200).json({ paymentResult: 'success' });
+          res.status(200).json({ paymentResult: "success" });
         } else {
           res.status(400).json({ error: "Failed to update payment details" });
         }
@@ -237,14 +238,107 @@ async function run() {
         res.status(500).json({ error: "Internal server error" });
       }
     });
-    
-    
-    
+
+    // get all asset
+    app.get("/assets", async (req, res) => {
+      try {
+        const assets = await assetsCollection.find().toArray();
+        res.status(200).json(assets);
+      } catch (error) {
+        res.status(500).json({ error: "Failed to fetch assets" });
+      }
+    });
+
+    // add an asset
+    app.post("/assets", async (req, res) => {
+      try {
+        const { productName, productType, productQuantity } = req.body;
+
+        // Create a new asset object
+        const newAsset = {
+          assetName: productName,
+          assetType: productType,
+          quantity: parseInt(productQuantity), // Convert quantity to integer
+          dateAdded: new Date(), // Add current date as the date added
+        };
+
+        // Insert the new asset into the database
+        const result = await assetsCollection.insertOne(newAsset);
+
+        if (result.insertedId) {
+          res.status(201).json({
+            message: "Asset added successfully",
+            assetId: result.insertedId,
+          });
+        } else {
+          res.status(500).json({ error: "Failed to add asset" });
+        }
+      } catch (error) {
+        console.error("Error adding asset:", error);
+        res.status(500).json({ error: "Internal server error" });
+      }
+    });
+
+    // get a specific asset
+    app.get("/assets/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      console.log(query);
+      try {
+        const asset = await assetsCollection.findOne(query);
+        if (asset) {
+          res.json(asset);
+        } else {
+          res.status(404).json({ message: "Asset not found" });
+        }
+      } catch (error) {
+        console.error("Error fetching asset:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
+
+    // Update asset
+    app.patch("/assets/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateData = req.body;
+      try {
+        const result = await assetsCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: updateData }
+        );
+
+        if (result.modifiedCount > 0) {
+          res.json({ modifiedCount: result.modifiedCount });
+        } else {
+          res.status(404).json({ message: "Asset not found" });
+        }
+      } catch (error) {
+        console.error("Error updating asset:", error);
+        res.status(500).json({ message: "Internal server error" });
+      } 
+    });
+
+    // delete asset
+    app.delete("/assets/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      try {
+        const deletedAsset = await assetsCollection.deleteOne(query);
+        
+        if (deletedAsset) {
+          res.status(200).json({ message: "Asset deleted successfully" });
+        } else {
+          res.status(404).json({ message: "Asset not found" });
+        }
+      } catch (error) {
+        console.error("Error deleting asset:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
 
 
-  } finally {
-    // Ensures that the client will close when you finish/error
-  }
+
+  } finally { }
 }
 run().catch(console.dir);
 
